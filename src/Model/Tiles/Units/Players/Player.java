57454b45f4ed6270160c3c7fc678cc81aa7bdf03
Mostafahosh <hosh8123@gmail.com>
2024-08-19 07:33:46 +0300

    FixLevelUpMethod

--- src/Model/Tiles/Units/Players/Player.java
+++ src/Model/Tiles/Units/Players/Player.java
@@ -40,7 +40,7 @@ public abstract class Player extends Units {
 //        }
 //    }
     public  void levelUp(){
-        this.experience += levelRequirment() ;
+        this.experience -= levelRequirment() ;
         this.level++;
         int healthGain = healthGain();
         int attackGain = attackGain();
